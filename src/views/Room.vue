bb4dfa7215faa83325ddd3545c56b3e2a73ad553
Boris Graeff <boris@viibe.co>
2020-04-05 23:33:55 +0200

    [ADD] set up home and room views

---
+++ src/views/Room.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="room">
+    <h1>Room: {{ slug }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    slug: {
+      type: String
+    }
+  }
+}
+</script>
